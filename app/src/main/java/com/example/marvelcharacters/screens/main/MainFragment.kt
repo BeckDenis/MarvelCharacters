1f4eee62cd0968b090694a0eed5430567148ac93
Денис Бекк <denisbeck555@gmail.com>
2021-07-12 23:54:06 +0300

    Add MainFragment

---
+++ app/src/main/java/com/example/marvelcharacters/screens/main/MainFragment.kt
@@ -0,0 +1,6 @@
+package com.example.marvelcharacters.screens.main
+
+import androidx.fragment.app.Fragment
+import com.example.marvelcharacters.R
+
+class MainFragment : Fragment(R.layout.fragment_main)
